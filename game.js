cc872688c6c2fd7c0bbd59ff0c081241430b9629
23127jk <23127jk@hvhs.school.nz>
2026-03-03 15:08:10 +1300

    layout

--- game.js
+++ game.js
@@ -1,11 +1,24 @@
 function setup() {
-console.log("setup:")
-cnv = new canva(600, 600);
+   cnv = new Canvas(800, 800);
+   console.log("setup:");
+   world.gravity.y = 10;
+
+    wallLH = new Sprite(0, height / 2, 8, height, 'k');
+
+    wallLH.color = 'black';
+
+    wallRH = new Sprite(800, 400, 25, 800, 'k');
+
+    wallTop = new Sprite(400, 0, 800, 25, 'k');
+
+    wallBot = new Sprite(400, 800, 800, 25, 'k');
 
 
 
 
 
+}
 
-
+function draw() {
+    background(255, 100, 100);
 }
