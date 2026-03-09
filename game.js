6e1cd5208f93e1c708d17cfb1a30bed8214071ea
23127jk <23127jk@hvhs.school.nz>
2026-03-09 13:04:43 +1300

    movement

--- game.js
+++ game.js
@@ -5,24 +5,34 @@ function setup() {
 
   wallLH = new Sprite(0, height / 2, 8, height, 'k');
   wallLH.color = 'black';
+  wallLH.bounciness=0;
   wallRH = new Sprite(800, 400, 25, 800, 'k');
+  wallRH.bounciness= 0;
   wallTop = new Sprite(400, 0, 800, 25, 'k');
+  wallTop.bounciness= 0;
   wallBot = new Sprite(400, 800, 800, 25, 'k');
+wallBot.bounciness=0;
 
-  player = new Sprite(725, 725, 65, 65, 'd')
+  player = new Sprite(755, 755, 65, 65, 'd')
   player.color = 'eef3f5'
   player.friction = 0;
+    player.bounciness= 0;
   //
   player.collider = 'dynamic';
   player.rotationlock = true;
   //
   platform_1 = new Sprite(400, 400, 425, 10, 'k');
   platform_1.friction = 0;
-
+platform_1.bounciness = 0;
   platform_2 = new Sprite(600, 200, 450, 10, 'k')
+  platform_2.bounciness= 0;
   wallRH_2 = new Sprite(600, 700, 25, 600, 'k')
-
+  wallRH_2.bounciness=0;
   platform_3 = new Sprite(200, 600, 450, 10, 'k')
+  platform_3.bounciness = 0;
+  platform_4 = new Sprite(735, 600, 100, 10, 'k')
+  platform_4.bounciness= 0;
+  platform_4.drag =0;
 }
 
 function draw() {
@@ -30,16 +40,30 @@ function draw() {
   //movement making it to one side very quickly and not go diagonal.
  // player.vel.x = 0;
  // player.vel.y = 0;
-  if (kb.pressing('up')) {
-    player.vel.y = -300;
+ if (kb.pressing('up') && !kb.pressing('down') && !kb.pressing('left') && !kb.pressing('right')) {
+    player.vel.y = -50;
   }
-  else if (kb.pressing('down')) {
-    player.vel.y = 300;
+  else if(kb.released('up')){
+    player.vel.y = -50
   }
- else if (kb.pressing('left')) {
-    player.vel.x = -300;
+if (kb.pressing('down') && !kb.pressing('up') && !kb.pressing('right') && !kb.pressing('left')) {
+    player.vel.y = 50;
   }
-  else if (kb.pressing('right')) {
-    player.vel.x = 300;
+ else if (kb.released('down')){
+    player.vel.y = 50;
   }
+  if (kb.pressing('left') && !kb.pressing('up') && !kb.pressing('right') && !kb.pressing('down')) {
+    player.vel.x = -50;
+  }
+  else if (kb.released('left')){
+player.vel.x = -50;
+  }
+if (kb.pressing('right') && !kb.pressing('up') && !kb.pressing('down') && !kb.pressing('left')) {
+    player.vel.x = 50;
+  }
+  else if (kb.released('right')){
+    player.vel.x= 50;
+  }
+  
 }
+
